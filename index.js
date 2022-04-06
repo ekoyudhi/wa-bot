b8a24f3d05e682832f8a9bea73d54b749eb86539
ekoyudhi <ekoyudhiprastowo@gmail.com>
2022-04-06 07:03:38 +0700

    add keep alive

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ function start(client) {
           console.error('Error when sending: ', erro); //return object error
         });
     } else if (message.isGroupMsg === true) {
-      console.log(message);
+      //console.log(message);
       client
-        .sendText(message.from, message.text)
+        .sendText(message.from, message.body)
         .then((result) => {
           console.log('Result: ', result);
         })
@@ -43,5 +43,54 @@ function start(client) {
           console.log("Error: ", erro);
         });
     }
+  });
+
+  // function to detect conflits and change status
+  // Force it to keep the current session
+  // Possible state values:
+  // CONFLICT
+  // CONNECTED
+  // DEPRECATED_VERSION
+  // OPENING
+  // PAIRING
+  // PROXYBLOCK
+  // SMB_TOS_BLOCK
+  // TIMEOUT
+  // TOS_BLOCK
+  // UNLAUNCHED
+  // UNPAIRED
+  // UNPAIRED_IDLE
+  client.onStateChange((state) => {
+    console.log('State changed: ', state);
+    // force whatsapp take over
+    if ('CONFLICT'.includes(state)) client.useHere();
+    // detect disconnect on whatsapp
+    if ('UNPAIRED'.includes(state)) console.log('logout');
+  });
+
+  // DISCONNECTED
+  // SYNCING
+  // RESUMING
+  // CONNECTED
+  let time = 0;
+  client.onStreamChange((state) => {
+    console.log('State Connection Stream: ' + state);
+    clearTimeout(time);
+    if (state === 'DISCONNECTED' || state === 'SYNCING') {
+      time = setTimeout(() => {
+        client.close();
+      }, 80000);
+    }
+  });
+
+  // function to detect incoming call
+  client.onIncomingCall(async (call) => {
+    console.log(call);
+    client.sendText(call.peerJid, "Sorry, I still can't answer calls");
+  });
+
+  // Catch ctrl+C
+  process.on('SIGINT', function() {
+    client.close();
   });
 }
